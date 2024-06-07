07c77b3ed58f63885a9f2a2604f7e0caa39203ef
JesusGarcia <jesusg0113@gmail.com>
2024-06-07 20:06:20 -0300

    Pagina cliente finalizada, pagina admin restaurant en progreso

---
+++ src/utils/index.ts
@@ -0,0 +1,8 @@
+
+//Formato para mostrarlo como usd
+export function formatCurrency(amount: number) {
+    return new Intl.NumberFormat('en-US', {
+        style: 'currency',
+        currency: 'USD'
+    }).format(amount)
+}
